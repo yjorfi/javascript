496c7da744107d927dfbfa22e956ae95734d9657
yjorfi <9024527@student.zadkine.nl>
2024-11-06 15:46:33 +0100

    opdracht 11

--- Opdracht 10/script.js
+++ Opdracht 10/script.js
@@ -1,16 +1,39 @@
-let getal_1 = document.getElementById("getal-1")
-let getal_2 = document.getElementById("getal-2")
-let antwoord = document.getElementById("antwoord")
+let getal_1 = document.getElementById("getal-1");
+let getal_2 = document.getElementById("getal-2");
+let antwoord = document.getElementById("antwoord");
 
 function multiply() {
-    antwoord.innerText = getal_1.value * getal_2.value
+    let result = getal_1.value * getal_2.value;
+    if (result < 0) {
+        antwoord.innerText = "antwoord onder de 0";
+    } else {
+        antwoord.innerText = result;
+    }
 }
+
 function add() {
-    antwoord.innerText = Number(getal_1.value) + Number(getal_2.value)
+    let result = Number(getal_1.value) + Number(getal_2.value);
+    if (result < 0) {
+        antwoord.innerText = "antwoord onder de 0";
+    } else {
+        antwoord.innerText = result;
+    }
 }
+
 function divide() {
-    antwoord.innerText = getal_1.value / getal_2.value
+    let result = getal_1.value / getal_2.value;
+    if (result < 0) {
+        antwoord.innerText = "antwoord onder de 0";
+    } else {
+        antwoord.innerText = result;
+    }
 }
+
 function minus() {
-    antwoord.innerText = getal_1.value - getal_2.value
+    let result = getal_1.value - getal_2.value;
+    if (result < 0) {
+        antwoord.innerText = "antwoord onder de 0";
+    } else {
+        antwoord.innerText = result;
+    }
 }
